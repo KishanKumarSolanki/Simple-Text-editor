65adc2a45a6bd4d6177c389531d4ed15b9c58b7f
KishanKumarSolanki <kkishankumar9983@gmail.com>
2025-01-20 02:57:36 +0530

    Italic button add

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,7 +16,7 @@ export default function TextForm(props) {
 
     const handleClearClick = ()=>{ 
         let newText = '';
-        setText(newText);
+        setText(newText)
         props.showAlert("Text Cleared!", "success");
     }
 
@@ -37,22 +37,31 @@ export default function TextForm(props) {
     }
 
     const handleBold = () => {
-      setBold(!isBold);
+      setBold(!isBold)
+      props.showAlert("Text Blod!", "success");
     };
+
+    const handleItalic = () => {
+        setItalic(!isItalic);
+        props.showAlert("Text Italic!", "success");
+      };
     
+
     const [isBold, setBold] = useState('');
+    const[isItalic,setItalic] = useState('');
     const [text, setText] = useState(''); 
     return (
         <>
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
             <h1 className='mb-4'>{props.heading}</h1>
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743' , fontWeight: isBold ? 'bold' : 'normal'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743' , fontWeight: isBold ? 'bold' : 'normal' , fontStyle : isItalic ? 'Normal' : 'Italic'}} id="myBox" rows="8"></textarea>
             </div>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleBold}>Text Bold</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleItalic}>Text Italic</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
         </div>
